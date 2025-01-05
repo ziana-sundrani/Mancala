7e688560f7464ee004b4f226581708ff28e4b61b
ziana-sundrani <zianasundrani@gmail.com>
2025-01-05 14:39:33 -0600

    Update RunGame.java

--- hw09_local_temp/src/main/java/org/cis1200/RunGame.java
+++ hw09_local_temp/src/main/java/org/cis1200/RunGame.java
@@ -5,8 +5,7 @@ import javax.swing.*;
 public class RunGame {
     /**
      * Main method run to start and run the game. Initializes the runnable game
-     * class of your choosing and runs it. IMPORTANT: Do NOT delete! You MUST
-     * include a main method in your final submission.
+     * class and runs it.
      */
     public static void main(String[] args) {
         // Set the game you want to run here
